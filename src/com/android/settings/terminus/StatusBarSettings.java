eb755f59c9dc2e3d5c992cc5051a587f6336582a
Dave Kessler <activethrasher00@gmail.com>
2015-01-13 21:05:59 -0500

    Status Bar Clock: rewrite for lollipop and add left clock (2/2)
    Add clock mods to cm-12.0 with new left position
    
    [mikeioannina]: Adapt for CyanogenMod

--- src/com/android/settings/terminus/StatusBarSettings.java
+++ src/com/android/settings/terminus/StatusBarSettings.java
@@ -2,6 +2,8 @@
 package com.android.settings.terminus;
 
 import android.content.ContentResolver;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -11,6 +13,8 @@ import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.text.format.DateFormat;
+import android.view.View;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -34,11 +38,18 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     // Quick Pulldown
     public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
+    // Cendter clock
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+
     // Statusbar battery percent
     private ListPreference mStatusBarBattery;
     private ListPreference mStatusBarBatteryShowPercent;
     // Quick Pulldown
     private SwitchPreference mStatusBarQuickQsPulldown;
+    // Center clock
+    private ListPreference mStatusBarClock;
+    private ListPreference mStatusBarAmPm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,6 +82,39 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                 .getApplicationContext().getContentResolver(),
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0) == 1));
         mStatusBarQuickQsPulldown.setOnPreferenceChangeListener(this);
+
+        // Center clock
+        mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
+        mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
+
+        int clockStyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK, 1);
+        mStatusBarClock.setValue(String.valueOf(clockStyle));
+        mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        mStatusBarClock.setOnPreferenceChangeListener(this);
+
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+        } else {
+            int statusBarAmPm = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_AM_PM, 2);
+            mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
+            mStatusBarAmPm.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Adjust clock position for RTL if necessary
+        Configuration config = getResources().getConfiguration();
+        if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+                mStatusBarClock.setEntries(getActivity().getResources().getStringArray(
+                        R.array.status_bar_clock_style_entries_rtl));
+                mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -94,6 +138,20 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) objValue;
             Settings.System.putInt(resolver,
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, value ? 1 : 0);
+            return true;
+        } else if (preference == mStatusBarClock) {
+            int clockStyle = Integer.parseInt((String) objValue);
+            int index = mStatusBarClock.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_CLOCK_STYLE, clockStyle);
+            mStatusBarClock.setSummary(mStatusBarClock.getEntries()[index]);
+            return true;
+        } else if (preference == mStatusBarAmPm) {
+            int statusBarAmPm = Integer.valueOf((String) objValue);
+            int index = mStatusBarAmPm.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
             return true;
         }
         return false;
