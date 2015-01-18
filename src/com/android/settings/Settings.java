snak3ater/platform_packages_apps_Settings

QS: Longclick action [2/2] Long press expanded status bar Settings button opens tiles settings
Thanks to @romanbb for status bar weather code & @Altaf-Mahdi for Status Bar Header haptic feedback option

Signed-off-by: snak3ater <saeedyasir91@gmail.com>

src/com/android/settings/Settings.java
@@ -19,6 +19,7 @@ package com.android.settings;
 import com.android.settings.applications.AppOpsSummary;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.simpleaosp.DisplayRotation;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 
 /**
  * Top-level Settings activity
@@ -105,5 +106,6 @@ public class Settings extends SettingsActivity {
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -114,6 +114,7 @@ import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.simpleaosp.DisplayRotation;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -301,7 +302,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),
-	    DisplayRotation.class.getName()
+	    DisplayRotation.class.getName(),
+	    QSTiles.class.getName()
     };
 
 

lichti1901/android_packages_apps_settings

src/com/android/settings/Settings.java
@@ -17,6 +17,7 @@
 package com.android.settings;
 
 import com.android.settings.applications.AppOpsSummary;
+import com.android.settings.terminus.qs.QSTiles;
 
 /**
  * Top-level Settings activity
@@ -101,5 +102,6 @@ public class Settings extends SettingsActivity {
 
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -112,6 +112,7 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.terminus.qs.QSTiles;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -297,7 +298,8 @@ public class SettingsActivity extends Activity
             AppNotificationSettings.class.getName(),
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
-            ApnSettings.class.getName()
+            ApnSettings.class.getName(),
+	    QSTiles.class.getName()
     };
 
 