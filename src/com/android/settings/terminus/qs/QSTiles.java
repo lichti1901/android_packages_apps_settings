c0a090ce47a7ffc26f817855faedb10687573f04
Danny Baumann <dannybaumann@web.de>
2015-01-23 10:50:47 +0100

    Cleanup QS.

--- src/com/android/settings/terminus/qs/QSTiles.java
+++ src/com/android/settings/terminus/qs/QSTiles.java
@@ -50,7 +50,7 @@ public class QSTiles extends Fragment implements
     private DraggableGridView mDraggableGridView;
     private View mAddDeleteTile;
     private boolean mDraggingActive;
-    private Resources mSystemUIResources;
+    private Context mSystemUiContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -64,12 +64,7 @@ public class QSTiles extends Fragment implements
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        try {
-            Context context = getActivity().createPackageContext(Utils.SYSTEM_UI_PACKAGE_NAME, 0);
-            mSystemUIResources = context.getResources();
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+        mSystemUiContext = Utils.createPackageContext(getActivity(), "com.android.systemui");
 
         ContentResolver resolver = getActivity().getContentResolver();
         String order = Settings.Secure.getString(resolver, Settings.Secure.QS_TILES);
@@ -190,7 +185,8 @@ public class QSTiles extends Fragment implements
             }
         });
 
-        final QSListAdapter adapter = new QSListAdapter(getActivity(), tilesList);
+        final QSListAdapter adapter = new QSListAdapter(getActivity(),
+                mSystemUiContext, tilesList);
         new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.add_qs)
                 .setSingleChoiceItems(adapter, -1, selectionListener)
@@ -225,7 +221,7 @@ public class QSTiles extends Fragment implements
 
         if (item.name != null) {
             ImageView icon = (ImageView) qsTile.findViewById(android.R.id.icon);
-            Drawable d = Utils.getNamedDrawableFromSystemUI(mSystemUIResources, item.resourceName);
+            Drawable d = Utils.getNamedDrawable(mSystemUiContext, item.resourceName);
             d.setColorFilter(getResources().getColor(R.color.qs_tile_tint_color),
                     PorterDuff.Mode.SRC_ATOP);
             icon.setImageDrawable(d);
